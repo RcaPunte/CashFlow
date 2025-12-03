0d27f945efc9ab8139d169a7558a4be6a487c025
RcaPunte <dev@Leroms-MacBook-Air.local>
2025-12-03 23:04:51 +0530

    1st

--- lib/account/controller/ledger_controller.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import '../../cash_book/controller/cash_book_controller.dart';
-
-// final ledgerProvider = FutureProvider<Map<String, double>>((ref) async {
-//   final controller = ref.watch(entriesControllerProvider);
-//   return controller.fetchLedgerTotals();
-// });
--- lib/cash_book/controller/cash_book_filter.dart
+++
@@ -1,6 +0,0 @@
-import 'package:cashledger/cash_book/model/cash_book_filter.dart';
-import 'package:flutter_riverpod/legacy.dart';
-
-final cashbookFilterProvider = StateProvider.autoDispose<CashbookFilter>(
-  (ref) => CashbookFilter(),
-);
